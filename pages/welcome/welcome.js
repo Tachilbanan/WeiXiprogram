7f72c880786505d98136e23baa7fc795fbbc69a1
Tachilbanan <727891715@qq.com>
2018-07-24 17:37:32 +0800

    欢迎页面和新闻页面绑定

--- pages/welcome/welcome.js
+++ pages/welcome/welcome.js
@@ -1,3 +1,32 @@
 Page({
+  onTap:function(event){
+    // wx.navigateTo({
+    //   url: '../posts/post',
+    // })
 
+    wx.redirectTo({
+      url: '../posts/post',
+    })
+    
+    // wx.navigateTo({
+    //   url: 'String',
+    //   success:function(res){
+    //     //success
+    //   },
+    //   fail:function(){
+    //     //fail
+    //   },
+    //   complete:function(){
+    //     //complete
+    //   }
+    // })
+  },
+
+  onUnload:function(){
+    console.log("onUnload");
+  },
+
+  onHide:function(){
+    console.log("welcome page is hide");
+  }
 })
