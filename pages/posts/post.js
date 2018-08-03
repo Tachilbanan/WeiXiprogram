f3f988edb7a69c55957639e939ea4f33fff0045b
Tachilbanan <727891715@qq.com>
2018-08-03 15:15:31 +0800

    完成tab的导入，完成电影页面三个template的嵌套（缺少样式）

--- pages/posts/post.js
+++ pages/posts/post.js
@@ -15,5 +15,22 @@ Page({
     wx.navigateTo({
       url: 'post-detail/post-detail?id=' + postId
     })
+  },
+
+  // onSwiperItemTap:function(event){
+  //   var postId = event.currentTarget.dataset.postid;
+  //   wx.navigateTo({
+  //     url: 'post-detail/post-detail?id=' + postId
+  //   })
+  // },
+
+  onSwiperTap:function(event){
+    //target和currentTarget区别
+    //target指的是当前点击的组件，currentTarget指的是事件捕捉的组件
+    //target这里指的时image，currentTarget这指的是swiper
+    var postId = event.target.dataset.postid;
+    wx.navigateTo({
+      url: 'post-detail/post-detail?id=' + postId
+    })
   }
 })
